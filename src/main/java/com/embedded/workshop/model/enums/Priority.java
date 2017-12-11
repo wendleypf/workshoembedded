10e6ec9177886542b37f2ef6d3ea95c88f8743ac
Wendley Paulo de França <wendley.franca@embedded-3.virtus.ufcg.edu.br>
2017-12-11 18:49:47 -0300

    erro

--- src/main/java/com/embedded/workshop/model/enums/Priority.java
+++
@@ -1,5 +0,0 @@
-package com.embedded.workshop.model.enums;
-
-public enum Priority {
-    HIGH, AVERAGE, LOW;
-}
--- src/main/java/com/embedded/workshop/model/enums/State.java
+++
@@ -1,5 +0,0 @@
-package com.embedded.workshop.model.enums;
-
-public enum State {
-    TODO, ON_GOING, REVIEW, DONE;
-}
